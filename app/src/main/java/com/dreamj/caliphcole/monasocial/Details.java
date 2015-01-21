1ba4063e937c9b92c28781979f1ba1e6959e11fa
wizcalil <colecaliph@gmail.com>
2015-01-21 05:13:37 -0500

    Initial commit

---
+++ app/src/main/java/com/dreamj/caliphcole/monasocial/Details.java
@@ -0,0 +1,23 @@
+package com.dreamj.caliphcole.monasocial;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by CaliphCole on 01/21/2015.
+ */
+public class Details extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.details, container, false);
+
+
+
+        return rootView;
+    }
+}
